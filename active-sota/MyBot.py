a95cd50de52a60607ee8585bf972f6159f9753f0
Shrivu Shankar <shrivu1122@gmail.com>
2018-11-26 22:55:20 -0600

    Setting Up New Technique

--- active-sota/MyBot.py
+++ active-sota/MyBot.py
@@ -6,23 +6,16 @@ import hlt
 import random
 import logging
 
-from ship_agent import ShipAgent, ship_agents
-from shipyard_agent import ShipyardAgent
+from halite_agent import HaliteAgent
 from bot_utils import *
 
-# from AlgoBot2 import play
+from AlgoBot2 import play
 
 game = hlt.Game()
 
-ShipAgent.load_model()
-ShipyardAgent.load_model()
-
-game.ready("PPOAlgoBot")
+game.ready("ConvBot")
 
 logging.info("[MAIN] Successfully created bot! My Player ID is {}.".format(game.my_id))
-
-shipyard_agent = ShipyardAgent(game)
-shipyard_agent.update(game_to_matrix(game))
 
 while True:
 
@@ -30,74 +23,15 @@ while True:
 
     me = game.me
     game_map = game.game_map
-    cur_ships = me.get_ships()
-    map_matrix = game_to_matrix(game)
 
-    cmds = []
+    algo_cmds = play(game)
+    logging.info(algo_cmds)
 
-    #### EXPERT ####
+    mat = game_to_matrix(game)
+    cmds_mat = commands_to_matrix(game, algo_cmds)
+    cmds = matrix_to_cmds(game, cmds_mat)
 
-    # algo_cmds = play(game)
-
-    #### SHIPS ####
-
-    logging.info('Ships: ' + str(len(cur_ships)))
-
-    ShipAgent.process_dead(cur_ships)
-
-    for ship in cur_ships:
-
-        if ship.id not in ship_agents:
-            ship_agents[ship.id] = ShipAgent(ship, game)
-
-        agent = ship_agents[ship.id]
-        agent.update(ship, map_matrix)
-        cmds.extend(agent.get_actions())
-
-    #### SHIPYARD ####
-
-    shipyard_agent.update(map_matrix)
-    cmds.extend(shipyard_agent.get_actions())
-
-    #### DONE ####
-
-    # algo_movements = {}
-    # ppo_movements = {}
-    #
-    # if 'g' in algo_cmds and 'g' not in cmds:
-    #     shipyard_agent.reword = -10
-    #     logging.info('!! g')
-    # elif 'g' not in algo_cmds and 'g' in cmds:
-    #     shipyard_agent.reword = -1
-    #     logging.info('!! g')
-    #
-    # for c in algo_cmds:
-    #     if c.startswith('m') and c not in cmds and 'o' not in c:
-    #         _, id, d = c.split(' ')
-    #         algo_movements[id] = d
-    #         logging.info('!! ' + c)
-    #
-    # for c in cmds:
-    #     if c.startswith('m') and c not in algo_cmds and 'o' not in c:
-    #         _, id, d = c.split(' ')
-    #         ppo_movements[id] = d
-    #         logging.info('!! ' + c)
-    #
-    # for ship_id in ppo_movements:
-    #
-    #     if ship_id not in algo_movements:
-    #         ship_agents[int(ship_id)].reword = -5
-    #     else:
-    #         ship_agents[int(ship_id)].reword = -1
-    #
-    # for ship_id in algo_movements:
-    #
-    #     if ship_id not in ppo_movements:
-    #         ship_agents[int(ship_id)].reword = -3
-    #     else:
-    #         ship_agents[int(ship_id)].reword = -1
-
+    logging.info(algo_cmds)
     logging.info(cmds)
-    # logging.info(algo_cmds)
 
-    game.end_turn(cmds)
+    game.end_turn(algo_cmds)
--- active-sota/bot_utils.py
+++ active-sota/bot_utils.py
@@ -6,6 +6,8 @@ import os
 
 from hlt.positionals import Position
 
+np.set_printoptions(threshold=np.nan)
+
 @contextmanager
 def import_quietly():
 
@@ -23,28 +25,17 @@ def import_quietly():
 
     sys.stderr = stderr
 
-def pick_action(pvals, temp=1.0):
-
-    preds = np.nan_to_num(pvals)
-    preds = np.log(preds + 1e-8) / temp
-    preds = np.exp(preds)
-    preds = preds / np.sum(preds)
-
-    pred = np.random.choice(pvals.shape[0], p=preds)
-
-    return pred
-
 def game_to_matrix(game):
 
     game_map = game.game_map
 
-    map_mat = np.zeros((5, game_map.height, game_map.width), np.float32)
+    map_mat = np.zeros((game_map.height, game_map.width, 5), np.float32)
 
-    # dropoffs
-    # my ships
-    # enemy dropoffs
-    # enemy ships
-    # halite
+    # 0 dropoffs/shipyards
+    # 1 my ships
+    # 2 enemy dropoffs/shipyards
+    # 3 enemy ships
+    # 4 halite
 
     for x in range(game_map.height):
 
@@ -55,22 +46,108 @@ def game_to_matrix(game):
 
             if cell.is_occupied:
                 if cell.ship.owner == game.my_id:
-                    map_mat[1, y, x] = 1
+                    map_mat[y, x, 1] = 1
                 else:
-                    map_mat[3, y, x] = 1
+                    map_mat[y, x, 3] = 1
 
             if cell.has_structure:
                 if cell.structure.owner == game.my_id:
-                    map_mat[0, y, x] = 1
+                    map_mat[y, x, 0] = 1
                 else:
-                    map_mat[2, y, x] = 1
+                    map_mat[y, x, 2] = 1
 
-            map_mat[4, y, x] = cell.halite_amount
+            map_mat[y, x, 4] = cell.halite_amount
 
-    # average halite
-    map_mat[4, :, :] = map_mat[4, :, :] / np.mean(map_mat[4, :, :])
+    # norm halite
+    map_mat[:, :, 4] = map_mat[:, :, 4] / 500
 
     return map_mat
+
+def commands_to_matrix(game, cmds):
+
+    game_map = game.game_map
+    ships = game.me.get_ships()
+    shipyard_pos = game.me.shipyard.position
+
+    cmd_mat = np.zeros((game_map.height, game_map.width, 6), np.float32)
+
+    # 0 stay
+    # 1 spawn/dropoff
+    # 2 up
+    # 3 down
+    # 4 left
+    # 5 right
+
+    for ship in ships:
+
+        move = None
+        for cmd in cmds:
+            if str(ship.id) in cmd and not cmd.endswith('o'):
+                move = cmd
+                break
+
+        if move:
+            _, _, dir = move.split(' ')
+            i = {'n': 2, 's': 3, 'w': 4, 'e': 5}[dir]
+        else:
+            i = 0
+
+        pos = ship.position
+        cmd_mat[pos.y, pos.x, i] = 1
+
+    if 'g' in cmds:
+        cmd_mat[shipyard_pos.y, shipyard_pos.x, 1] = 1
+    else:
+        cmd_mat[shipyard_pos.y, shipyard_pos.x, 0] = 1
+
+    empty_idx = np.where(cmd_mat[:, :] == [0]*6)
+    cmd_mat[empty_idx[0], empty_idx[1], 0] = 1
+
+    return cmd_mat
+
+def matrix_to_cmds(game, matrix):
+
+    game_map = game.game_map
+    ships = game.me.get_ships()
+    shipyard_pos = game.me.shipyard.position
+
+    cmds = []
+
+    # 0 stay
+    # 1 spawn/dropoff
+    # 2 up
+    # 3 down
+    # 4 left
+    # 5 right
+
+    for x in range(game_map.height):
+
+        for y in range(game_map.width):
+
+            vec = matrix[y, x]
+
+            # make one hot
+
+            if vec[0] == 1:
+                continue
+            elif vec[1] == 1:
+                if x == shipyard_pos.x and y == shipyard_pos.y:
+                    cmds.append('g')
+                else:
+                    pass # dropoff logic
+            else:
+                cur_ship = None
+                for ship in ships:
+                    pos = ship.position
+                    if x == pos.x and y == pos.y:
+                        cur_ship = ship
+                        break
+                if vec[2] == 1: cmds.append(f'm {ship.id} n')
+                if vec[3] == 1: cmds.append(f'm {ship.id} s')
+                if vec[4] == 1: cmds.append(f'm {ship.id} w')
+                if vec[5] == 1: cmds.append(f'm {ship.id} e')
+
+    return cmds
 
 def roll_to_position(entity, mat, crop=4):
 
